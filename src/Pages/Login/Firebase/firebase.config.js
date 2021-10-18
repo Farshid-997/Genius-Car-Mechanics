413b286e68f4323c90d78badd762c7edbb6a6505
Farshid-997 <arnob.farshid@gmail.com>
2021-10-18 16:20:57 +0600

    first commit

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+    apiKey: "AIzaSyAKIkji8t4dQhhDt3ZkdgpvtkII84_c-5o",
+    authDomain: "car-mechanics-8ad10.firebaseapp.com",
+    projectId: "car-mechanics-8ad10",
+    storageBucket: "car-mechanics-8ad10.appspot.com",
+    messagingSenderId: "438890863311",
+    appId: "1:438890863311:web:e044968cddc7a9793c35ba"
+};
+
+export default firebaseConfig;
